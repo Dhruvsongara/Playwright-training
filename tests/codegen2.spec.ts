a912e5ed85b5d200f9703bf836f685b8a41d80cd
Dhruvsongara <dhruvsongara16@gmail.com>
2026-05-28 16:08:27 +0530

    codegensecondcode

---
+++ tests/codegen2.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.rahulshettyacademy.com/client/#/auth/login');
+  await expect(page.getByRole('heading', { name: 'Log in' })).toBeVisible();
+  await page.getByRole('textbox', { name: 'email@example.com' }).click();
+  await page.getByRole('textbox', { name: 'email@example.com' }).fill('dhruv123@gmail.com');
+  await page.getByRole('textbox', { name: 'enter your passsword' }).click();
+  await page.getByRole('textbox', { name: 'enter your passsword' }).fill('Dhruv@123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('ADIDAS ORIGINAL')).toBeVisible();
+  await page.getByRole('button', { name: ' Add To Cart' }).nth(2).click();
+  await page.getByRole('button', { name: ' Add To Cart' }).first().click();
+  await page.getByRole('button', { name: '   Cart' }).click();
+  await expect(page.getByRole('heading', { name: 'My Cart' })).toBeVisible();
+  await expect(page.locator('app-profile')).toContainText('iphone 13 pro');
+  await expect(page.locator('app-profile')).toContainText('ADIDAS ORIGINAL');
+  await page.getByRole('button', { name: 'Checkout❯' }).click();
+  await expect(page.locator('section')).toContainText('dhruv123@gmail.com');
+  await page.getByRole('textbox', { name: 'Select Country' }).click();
+  await page.getByRole('textbox', { name: 'Select Country' }).pressSequentially('ind');
+  await page.getByRole('button', { name: ' India' }).click();
+  await page.getByRole('combobox').first().selectOption('09');
+  await page.getByRole('combobox').nth(1).selectOption('27');
+  await page.getByRole('textbox').nth(1).click();
+  await page.getByRole('textbox').nth(1).fill('123');
+  await page.getByRole('textbox').nth(2).click();
+  await page.getByRole('textbox').nth(2).fill('Dhruv');
+  await page.getByText('Place Order').click();
+  await expect(page.getByRole('heading', { name: 'Thankyou for the order.' })).toBeVisible();
+  await expect(page.locator('#htmlData')).toContainText('iphone 13 pro');
+  await expect(page.locator('#htmlData')).toContainText('ADIDAS ORIGINAL');
+});
