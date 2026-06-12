0a4333c251e823b9d9adcd3c4602ca8108058ae9
Dhruvsongara <dhruvsongara16@gmail.com>
2026-06-12 17:04:26 +0530

    unicorn_update

--- tests/unicornProductValidate.spec.ts
+++ tests/unicornProductValidate.spec.ts
@@ -1,14 +1,14 @@
-import {test} from "@playwright/test";
-import {UnicornHome} from "../pages/UnicornHome";
-import {IphoneInfoPage} from "../pages/IphoneInfo";
-import {AirpodInfoPage } from "../pages/airpodInfo";
-import {CartPage} from "../pages/CartPage";
-import {StudentOffer} from "../pages/studentOffer";
-import dataFile from "../test-data/dataFile.json"
+// import {test} from "@playwright/test";
+// import {UnicornHome} from "../pages/UnicornHome";
+// import {IphoneInfoPage} from "../pages/IphoneInfo";
+// import {AirpodInfoPage } from "../pages/airpodInfo";
+// import {CartPage} from "../pages/CartPage";
+// import {StudentOffer} from "../pages/studentOffer";
+// import dataFile from "../test-data/dataFile.json"
 
-let productPage: UnicornHome;
-let cartPage: CartPage;
-let studentOffer: StudentOffer;
+// let productPage: UnicornHome;
+// let cartPage: CartPage;
+// let studentOffer: StudentOffer;
 
 
 // //flow while staying logged out
@@ -28,65 +28,65 @@ let studentOffer: StudentOffer;
 // });
 
 
-//flow with logged in account
-test.describe("Logged-in User flows", () =>{
+// //flow with logged in account
+// test.describe("Logged-in User flows", () =>{
         
-    test.beforeEach(async ({page})=>{
+//     test.beforeEach(async ({page})=>{
 
-        productPage = new UnicornHome(page);
-        cartPage = new CartPage(page);
+//         productPage = new UnicornHome(page);
+//         cartPage = new CartPage(page);
 
-        //page launch
-        await productPage.unicornURL();
-        await productPage.pageCheck();
-        await productPage.login(dataFile.login.email,dataFile.login.password);
-    });
+//         //page launch
+//         await productPage.unicornURL();
+//         await productPage.pageCheck();
+//         await productPage.login(dataFile.login.email,dataFile.login.password);
+//     });
 
-    test ("validate iphone 17 device search", async ({page}) => {
+//     test ("validate iphone 17 device search", async ({page}) => {
 
-        const iphoneInfo = new IphoneInfoPage(page);
+//         const iphoneInfo = new IphoneInfoPage(page);
 
-        //product selection
-        // await productPage.searchProduct('iphone 17');
-        await productPage.searchProduct(dataFile.searchItems.mobile);
-        await iphoneInfo.verifyPageStability();
-        await iphoneInfo.productClick();
-        await iphoneInfo.mrpVisibilityCheck();
-        await iphoneInfo.titleCheck();
-        await iphoneInfo.colorSelection();
-        await iphoneInfo.storageSelection();
-        await iphoneInfo.addToCart();
+//         //product selection
+//         // await productPage.searchProduct('iphone 17');
+//         await productPage.searchProduct(dataFile.searchItems.mobile);
+//         await iphoneInfo.verifyPageStability();
+//         await iphoneInfo.productClick();
+//         await iphoneInfo.mrpVisibilityCheck();
+//         await iphoneInfo.titleCheck();
+//         await iphoneInfo.colorSelection();
+//         await iphoneInfo.storageSelection();
+//         await iphoneInfo.addToCart();
 
-        //dealing with cart
-        // await cartPage.successfulAddtoCartCheck();
-        await cartPage.cartButtonClick();
-        await cartPage.proceedcheckOut();
-        await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
-        await cartPage.finalCheckout();
-    });
+//         //dealing with cart
+//         // await cartPage.successfulAddtoCartCheck();
+//         await cartPage.cartButtonClick();
+//         await cartPage.proceedcheckOut();
+//         await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
+//         await cartPage.finalCheckout();
+//     });
 
-    test ("validate airpods search", async ({page})=>{
+//     test ("validate airpods search", async ({page})=>{
 
-        const airpodInfo = new AirpodInfoPage(page);
+//         const airpodInfo = new AirpodInfoPage(page);
 
-        //product selection
-        // await productPage.searchProduct('airpods');
-        await productPage.searchProduct(dataFile.searchItems.earbuds);
-        await airpodInfo.verifyPageStability();
-        await airpodInfo.productClick();
-        await airpodInfo.mrpVisibilityCheck();
-        await airpodInfo.titleCheck();
-        await airpodInfo.addToCart();
+//         //product selection
+//         // await productPage.searchProduct('airpods');
+//         await productPage.searchProduct(dataFile.searchItems.earbuds);
+//         await airpodInfo.verifyPageStability();
+//         await airpodInfo.productClick();
+//         await airpodInfo.mrpVisibilityCheck();
+//         await airpodInfo.titleCheck();
+//         await airpodInfo.addToCart();
 
-        //dealing with cart
-        // await cartPage.successfulAddtoCartCheck();
-        await cartPage.proceedcheckOut();
-        await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
-        await cartPage.finalCheckout();
-    }); 
+//         //dealing with cart
+//         // await cartPage.successfulAddtoCartCheck();
+//         await cartPage.proceedcheckOut();
+//         await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
+//         await cartPage.finalCheckout();
+//     }); 
 
 
-    test.afterEach(async ({}, testInfo) => {
-        console.log(`Test "${testInfo.title}" finished with status: ${testInfo.status}`);
-    });
-});
+//     test.afterEach(async ({}, testInfo) => {
+//         console.log(`Test "${testInfo.title}" finished with status: ${testInfo.status}`);
+//     });
+// });
--- tests/unicornProductValidatepractice.spec.ts
+++ tests/unicornProductValidatepractice.spec.ts
@@ -1,15 +1,15 @@
-import {test} from "@playwright/test";
-import {UnicornHome} from "../pages/UnicornHome";
-import {IphoneInfoPage} from "../pages/IphoneInfo";
-import {AirpodInfoPage } from "../pages/airpodInfo";
-import {CartPage} from "../pages/CartPage";
-import {StudentOffer} from "../pages/studentOffer";
-import dataFile from "../test-data/dataFile.json"
+// import {test} from "@playwright/test";
+// import {UnicornHome} from "../pages/UnicornHome";
+// import {IphoneInfoPage} from "../pages/IphoneInfo";
+// import {AirpodInfoPage } from "../pages/airpodInfo";
+// import {CartPage} from "../pages/CartPage";
+// import {StudentOffer} from "../pages/studentOffer";
+// import dataFile from "../test-data/dataFile.json"
 
-let isLoggedIn = false;
-let productPage: UnicornHome;
-let cartPage: CartPage;
-let studentOffer: StudentOffer;
+// let isLoggedIn = false;
+// let productPage: UnicornHome;
+// let cartPage: CartPage;
+// let studentOffer: StudentOffer;
 
 // test.only("Student Offer flow (while staying logged out))",async ({page,browserName})=>{
 
@@ -22,91 +22,92 @@ let studentOffer: StudentOffer;
 //     await productPage.pageCheck();
 //     await studentOffer.goToStudentSection();
 //     await studentOffer.chooseMac();
-//     await studentOffer.selectM4();
+//     await studentOffer.selectM4Category();
+//     await studentOffer.selectM4Laptop();
 //     await studentOffer.DetailsFillUp();
 // });
 
-test.describe("Logged-in User flows", () =>{
+// test.describe("Logged-in User flows", () =>{
         
-    test.describe.configure({ mode: 'serial' });
-    test.beforeEach(async ({page})=>{
+//     test.describe.configure({ mode: 'serial' });
+//     test.beforeEach(async ({page})=>{
 
-        productPage = new UnicornHome(page);
-        cartPage = new CartPage(page);
+//         productPage = new UnicornHome(page);
+//         cartPage = new CartPage(page);
 
-        //page launch
-        await productPage.unicornURL();
-        await productPage.pageCheck();
-        // await productPage.login(dataFile.login.email,dataFile.login.password);
+//         //page launch
+//         await productPage.unicornURL();
+//         await productPage.pageCheck();
+//         // await productPage.login(dataFile.login.email,dataFile.login.password);
 
-        if (!isLoggedIn) {
+//         if (!isLoggedIn) {
 
-            await productPage.login(
-                dataFile.login.email,
-                dataFile.login.password
-            );
+//             await productPage.login(
+//                 dataFile.login.email,
+//                 dataFile.login.password
+//             );
 
-            await page.context().storageState({
-                path: 'playwright/.auth/user.json'
-            });
+//             await page.context().storageState({
+//                 path: 'playwright/.auth/user.json'
+//             });
 
-            isLoggedIn = true;
-        }
-    });
+//             isLoggedIn = true;
+//         }
+//     });
 
-    test ("validate iphone 17 device search", async ({page}) => {
+//     test ("validate iphone 17 device search", async ({page}) => {
 
-        const iphoneInfo = new IphoneInfoPage(page);
+//         const iphoneInfo = new IphoneInfoPage(page);
 
-        //product selection
-        // await productPage.searchProduct('iphone 17');
-        await productPage.searchProduct(dataFile.searchItems.mobile);
-        await iphoneInfo.verifyPageStability();
-        await iphoneInfo.productClick();
-        await iphoneInfo.mrpVisibilityCheck();
-        await iphoneInfo.titleCheck();
-        await iphoneInfo.colorSelection();
-        await iphoneInfo.storageSelection();
-        await iphoneInfo.addToCart();
+//         //product selection
+//         // await productPage.searchProduct('iphone 17');
+//         await productPage.searchProduct(dataFile.searchItems.mobile);
+//         await iphoneInfo.verifyPageStability();
+//         await iphoneInfo.productClick();
+//         await iphoneInfo.mrpVisibilityCheck();
+//         await iphoneInfo.titleCheck();
+//         await iphoneInfo.colorSelection();
+//         await iphoneInfo.storageSelection();
+//         await iphoneInfo.addToCart();
 
-        //dealing with cart
-        // await cartPage.successfulAddtoCartCheck();
-        await cartPage.cartButtonClick();
-        await cartPage.proceedcheckOut();
-        await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
-        await cartPage.finalCheckout();
-    });
+//         //dealing with cart
+//         // await cartPage.successfulAddtoCartCheck();
+//         await cartPage.cartButtonClick();
+//         await cartPage.proceedcheckOut();
+//         await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
+//         await cartPage.finalCheckout();
+//     });
 
-    test ("validate airpods search", async ({browser})=>{
+//     test ("validate airpods search", async ({browser})=>{
 
-        const context = await browser.newContext({
-            storageState: 'playwright/.auth/user.json'
-        });
-        const page = await context.newPage();
+//         const context = await browser.newContext({
+//             storageState: 'playwright/.auth/user.json'
+//         });
+//         const page = await context.newPage();
 
-        await productPage.unicornURL();
-        await productPage.pageCheck();
-        const airpodInfo = new AirpodInfoPage(page);
+//         await productPage.unicornURL();
+//         await productPage.pageCheck();
+//         const airpodInfo = new AirpodInfoPage(page);
 
-        //product selection
-        // await productPage.searchProduct('airpods');
-        await productPage.searchProduct(dataFile.searchItems.earbuds);
-        await airpodInfo.verifyPageStability();
-        await airpodInfo.productClick();
-        await airpodInfo.mrpVisibilityCheck();
-        await airpodInfo.titleCheck();
-        await airpodInfo.addToCart();
+//         //product selection
+//         // await productPage.searchProduct('airpods');
+//         await productPage.searchProduct(dataFile.searchItems.earbuds);
+//         await airpodInfo.verifyPageStability();
+//         await airpodInfo.productClick();
+//         await airpodInfo.mrpVisibilityCheck();
+//         await airpodInfo.titleCheck();
+//         await airpodInfo.addToCart();
 
-        //dealing with cart
-        // await cartPage.successfulAddtoCartCheck();
-        await cartPage.proceedcheckOut();
-        await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
-        await cartPage.finalCheckout();
-        await context.close();
-    }); 
+//         //dealing with cart
+//         // await cartPage.successfulAddtoCartCheck();
+//         await cartPage.proceedcheckOut();
+//         await cartPage.orderDetailsFillUp(dataFile.orderDetails.firstname, dataFile.orderDetails.lastname, dataFile.orderDetails.email, dataFile.orderDetails.mobile, dataFile.orderDetails.address, dataFile.orderDetails.city, dataFile.orderDetails.pincode, dataFile.orderDetails.stateOption);
+//         await cartPage.finalCheckout();
+//         await context.close();
+//     }); 
 
 
-    test.afterEach(async ({}, testInfo) => {
-        console.log(`Test "${testInfo.title}" finished with status: ${testInfo.status}`);
-    });
-});
+//     test.afterEach(async ({}, testInfo) => {
+//         console.log(`Test "${testInfo.title}" finished with status: ${testInfo.status}`);
+//     });
+// });
